0186495a683208132202c13470a715c4c8e9ed19
josh <josh@josh-Inspiron-N5010.(none)>
2014-11-01 04:10:48 +0530

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Speak::Application.config.secret_token = 'cdbf886e4aa9fc8fda875384ab906cd72215588c76af7ab94f74908d22113f9afd7b02c982cb236a3bf2b539920519ea1aea689c1d1e656b6fae9ffdd4ac0c50'
